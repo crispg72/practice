7a3af486b8924cc3810d28f114505278af6e5912
Chris Green <cgreen12@bloomberg.net>
2022-04-19 13:44:27 +0100

    added calc_sub_squares

--- sudoku/python/solve.py
+++ sudoku/python/solve.py
@@ -16,6 +16,23 @@ def get_row_col_for_sub_square(grid, sub_square_id, sub_square_index):
     return row, col
 
 
+def calc_sub_squares(grid):
+    sub_squares = []
+    sub_square_size = int(math.sqrt(len(grid)))
+
+    for i in range(len(grid)):
+        sub_square = []
+        for n in range(len(grid)):
+            row, col = get_row_col_for_sub_square(grid, i, n)
+            print(row, col)
+            value = grid[row][col]
+            if value != "x":
+                sub_square.append(value)
+        sub_squares.append(sub_square)
+
+    return sub_squares
+
+
 def read_grid(filename):
     with open(sys.argv[1]) as f:
         lines = f.readlines()
@@ -29,7 +46,7 @@ def read_grid(filename):
 
 def main():
     grid = read_grid(sys.argv[1])
-    print(grid)
+    print(calc_sub_squares(grid))
 
 
 if __name__ == "__main__":
